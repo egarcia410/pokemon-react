0aa3b4ea0ac607d4ceae24cb17dbfaf3070e3b69
egarcia410 <egarcia410@gmail.com>
2018-01-12 13:57:17 -0600

    setup deployment

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const PokemonController = require('./controllers/PokemonController');
+const serveStatic = require('serve-static');
 
 module.exports = (app) => {
     app.get('/get-pokemon-by-name/:name', PokemonController.getPokemonByName);
     app.get('/get-pokemon-by-rarity/:rarity', PokemonController.getPokemonByRarity);
     app.get('/get-pokemon-by-id/:id', PokemonController.getPokemonById);
+    
+    app.use(serveStatic(__dirname + '/../build'));
+    
+    app.all('/*', function (req, res) {
+        res.sendfile(__dirname + '/../build/index.html');
+    })
 };
