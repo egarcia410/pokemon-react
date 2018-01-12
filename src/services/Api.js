7adfd31cf31d4b3ce2c22392570da40d385cea3d
egarcia410 <egarcia410@gmail.com>
2018-01-12 14:07:06 -0600

    Update api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 // Creates an instance of axios, setting the base URL to link up with the backend
 export default () => {
+    if (location.port) {
+        // For development
+        var url = 'http://localhost:8081'
+    } else {
+        // For production
+        var url = location.protocol + '//' + location.hostname;
+    }
     return axios.create({
-        baseURL: 'http://localhost:8081'
-    });
-};
+        baseURL: url
+    })
+}
